73ee653cd89d9967d8ab78104ec52c5785c2c21d
Alex Kazik <alex@kazik.de>
2023-05-16 17:56:49 +0200

    Internal: Add script to check some basics

---
+++ check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#
+# Perform a few simple checks ahead of a PR
+#
+
+# Usage: `./check.sh` or `./check.sh <toolchain>`
+# If the toolchain is omitted `+nightly` is used, `+stable` or `+beta` are the most common alternatives
+
+TOOLCHAIN=${1:-+nightly}
+echo Using toolchain $TOOLCHAIN
+
+# builds
+cargo $TOOLCHAIN build --release --tests || exit 1
+
+# clippy
+cargo $TOOLCHAIN clippy --release --tests -- -D warnings || exit 1
+
+# update formatting
+cargo $TOOLCHAIN fmt --all || exit 1
+
+# update readme
+cargo rdme --force || exit 1
+
+# create docs
+if test "$TOOLCHAIN" = "+nightly"
+then
+  RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc || exit 1
+else
+  echo "Skipping 'cargo doc' with doc_cfg since it's only available on nightly"
+fi
+
+# tests
+cargo $TOOLCHAIN test --release -- --include-ignored || exit 1
