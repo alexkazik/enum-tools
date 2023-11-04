5cd30e91665d1abb1b8d9c578b06ad0f14f8f160
Alex Kazik <alex@kazik.de>
2023-11-04 10:29:28 +0100

    Internal: Improve check.sh

--- check.sh
+++ check.sh
@@ -5,16 +5,25 @@
 #
 
 # Usage: `./check.sh` or `./check.sh <toolchain>`
-# If the toolchain is omitted `+nightly` is used, `+stable` or `+beta` are the most common alternatives
+# If the toolchain is omitted `+nightly`,`+stable` and `+1.58.1` is used, `+stable` or `+beta` are the most common alternatives
+
+TOOLCHAIN=${1:-+1.58.1}
+echo Using toolchain $TOOLCHAIN
+
+# use crates available at this rust version
+cargo $TOOLCHAIN update
+
+# builds
+cargo $TOOLCHAIN build --locked --release || exit 1
 
 TOOLCHAIN=${1:-+nightly}
 echo Using toolchain $TOOLCHAIN
 
 # builds
-cargo $TOOLCHAIN build --release --tests || exit 1
+cargo $TOOLCHAIN build --locked --release --tests || exit 1
 
 # clippy
-cargo $TOOLCHAIN clippy --release --tests -- -D warnings || exit 1
+cargo $TOOLCHAIN clippy --locked --release --tests -- -D warnings || exit 1
 
 # update formatting
 cargo $TOOLCHAIN fmt --all || exit 1
@@ -30,11 +39,14 @@ else
   echo "Skipping 'cargo doc' with doc_cfg since it's only available on nightly"
 fi
 
+TOOLCHAIN=${1:-+stable}
+echo Using toolchain $TOOLCHAIN
+
 # tests
-if test "$TOOLCHAIN" = "+nightly"
+if test "$TOOLCHAIN" != "+stable"
 then
-  cargo $TOOLCHAIN test --release || exit 1
+  cargo $TOOLCHAIN test --locked --release || exit 1
   echo "Skipping 'compile-fail' tests since it's maybe different on nightly, try '$0 +stable'"
 else
-  cargo $TOOLCHAIN test --release -- --include-ignored || exit 1
+  cargo $TOOLCHAIN test --locked --release -- --include-ignored || exit 1
 fi
