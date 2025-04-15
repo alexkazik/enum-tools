afa111bc20812a0436498720e404eefa7bf7b45a
Alex Kazik <alex@kazik.de>
2025-04-15 22:57:28 +0200

    Run compile-fail tests only when running on stable
    That's because the different rust channels do sometimes have different
    output and it checks the exact result.

--- tests/compile-fail.rs
+++ tests/compile-fail.rs
@@ -1,5 +1,9 @@
+use rustc_version::{version_meta, Channel};
+
 #[test]
 #[ignore]
 fn compile_fail() {
-    trybuild::TestCases::new().compile_fail("tests/compile-fail/*.rs");
+    if version_meta().is_ok_and(|v| v.channel == Channel::Stable) {
+        trybuild::TestCases::new().compile_fail("tests/compile-fail/*.rs");
+    }
 }
